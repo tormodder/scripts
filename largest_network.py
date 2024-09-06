a7cd672c6fb0db555eb42f59872bdf9dee0b0b97
tmod <tormofl@stud.ntnu.no>
2024-09-06 14:19:21 +0200

    add largest_network

---
+++ largest_network.py
@@ -0,0 +1,31 @@
+import ipaddress
+import argparse
+
+
+def largest_network(file):
+
+    addresses = []
+
+    with open(file) as f:
+        addresses = f.read().split("\n")
+
+    addresses = list(filter(None, addresses))
+
+    # Remove ipv6 and parse address strings
+    for k, v in enumerate(addresses):
+        if ":" in v:
+            addresses.pop(k)
+            continue
+        addresses[k] = v.strip()
+
+    networks = [ipaddress.ip_network(ip, strict=False) for ip in addresses]
+
+    largest_network = min(networks, key=lambda net: net.prefixlen)
+
+    return largest_network
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Find the largest network range from an arbitrary list of ipv6 addresses in CIDR notation")
+    parser.add_argument('file', type=str, help='path to the file of ipv6 addresses')
+    args = parser.parse_args()
+    largest_network(args.file)
